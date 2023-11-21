4dca4ea65d9071cb22330fd8a4afa35e2f9e66d7
LucasRioual <lucas.rioual@centrale-marseille.fr>
2023-11-21 14:36:32 +0100

    JoinScreen

---
+++ Screen/JoinScreen.js
@@ -0,0 +1,29 @@
+//Khaoula 
+import React from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, ScrollView} from 'react-native';
+
+
+
+
+const JoinScreen=() => {
+    return (
+
+      <View style={styles.ViewMain} >
+        
+          
+        
+      </View>
+      
+      
+    );
+}
+
+const styles = StyleSheet.create ({
+  ViewMain: {
+    flex: 1, 
+    backgroundColor: '#FFEBD7',
+  }
+  
+});
+
+export default JoinScreen;
