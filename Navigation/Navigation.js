b186e87338d7734400b7324579a03d1db119c751
KhaoulaBelaaziz <khaoula.belaaziz@centrale-marseille.fr>
2023-11-22 16:28:47 +0100

    Home

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../Screen/HomeScreen';
 import ModeScreen from '../Screen/ModeScreen';
 import SalonScreen from '../Screen/SalonScreen';
+import JoinScreen from '../Screen/JoinScreen';
 
 
 
@@ -17,6 +18,7 @@ const Navigation = () => {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Mode" component={ModeScreen} />
         <Stack.Screen name="Salon" component={SalonScreen} />
+        <Stack.Screen name="Join" component={JoinScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
